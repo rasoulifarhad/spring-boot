64f24e06ef24527cd615a3838a887f2d322c5b8c
Farhad rasouli <rasouli.farhad@gmail.com>
2023-07-22 08:58:42 +0330

    updated

--- spring-value-object/src/main/java/com/farhad/example/valueobjectsdemo/domain/PhoneNumber.java
+++ spring-value-object/src/main/java/com/farhad/example/valueobjectsdemo/domain/PhoneNumber.java
@@ -1,11 +1,27 @@
 package com.farhad.example.valueobjectsdemo.domain;
 
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
-@RequiredArgsConstructor
 @Value
 public class PhoneNumber {
     
+    private static final PhoneNumberUtil PHONE_NUMBER_UTIL = PhoneNumberUtil.getInstance();
     String value;
+
+    public PhoneNumber(String value) {
+        this.value = value;
+        validatePhoneNumber(value);
+    }
+
+    private static void validatePhoneNumber(String value) {
+
+        try {
+            if (Long.parseLong(value) <= 0) {
+                throw new PhoneNumberParsingException("The phone number must be positive: " + value);
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    
 }

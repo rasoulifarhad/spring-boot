bae30d252952b7077969d4dba3529188b85b00a1
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-01 19:55:11 +0330

    updated

--- springboot-wiremock-demo/src/test/java/com/farhad/example/wiremockdemo/SpringbootWiremockDemoApplicationTests.java
+++ springboot-wiremock-demo/src/test/java/com/farhad/example/wiremockdemo/SpringbootWiremockDemoApplicationTests.java
@@ -1,13 +1,60 @@
 package com.farhad.example.wiremockdemo;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class SpringbootWiremockDemoApplicationTests {
 
+	private static WireMockServer wireMockServer;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeAll
+	static void init() {
+		wireMockServer = new WireMockServer(
+			new WireMockConfiguration().port(7075)
+		);
+		wireMockServer.start();
+		WireMock.configureFor("localhost", 7075);
+	}
+
 	@Test
-	void contextLoads() {
+	void testCallHttpBinAnything() throws Exception {
+
+		Given: 
+		stubFor(WireMock.get(urlMatching("/anything"))
+			.willReturn(aResponse().withStatus(OK.value())));
+		
+		When: 
+		mockMvc.perform(get("/demo"))
+			.andExpect(status().isOk());
+
+		Then:
+		verify(getRequestedFor(urlPathEqualTo("/anything")));	
+
 	}
 
 }

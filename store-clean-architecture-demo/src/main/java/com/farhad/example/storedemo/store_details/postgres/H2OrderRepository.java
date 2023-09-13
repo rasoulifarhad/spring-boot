1fa21337c4ba7fe5f745e09e29ce41e7d894efef
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-13 07:03:57 +0330

    updated

--- store-clean-architecture-demo/src/main/java/com/farhad/example/storedemo/store_details/postgres/H2OrderRepository.java
+++ store-clean-architecture-demo/src/main/java/com/farhad/example/storedemo/store_details/postgres/H2OrderRepository.java
@@ -1,19 +1,26 @@
 package com.farhad.example.storedemo.store_details.postgres;
 
+import static java.util.stream.Collectors.toList;
+
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.farhad.example.storedemo.store_core.orders.Order;
+import com.farhad.example.storedemo.store_core.orders.OrderId;
 import com.farhad.example.storedemo.store_core.orders.OrderLineItem;
 import com.farhad.example.storedemo.store_core.orders.OrderRepository;
 import com.farhad.example.storedemo.store_core.security.PrincipalUser;
+import com.farhad.example.storedemo.store_core.variants.InventoryItem;
 import com.farhad.example.storedemo.store_details.postgres.mappers.OrderMapper;
 import com.farhad.example.storedemo.store_details.postgres.mappers.OrderMapper.LineItemMapper;
+import com.farhad.example.storedemo.store_details.postgres.mappers.OrderMapper.LineItemRow;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -67,8 +74,23 @@ public class H2OrderRepository implements OrderRepository {
 
 	@Override
 	public List<Order> listOrdersForUser(PrincipalUser user) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'listOrdersForUser'");
+		List<Order> orders = jdbcTemplate.query("select * from orders o where o.user_email = ?", orderMapper, user.getEmail());
+		Map<OrderId, Order> orderIdsMap = orders.stream().collect(Collectors.toMap(Order::getId,o -> o));
+
+		List<LineItemRow> items = 
+			jdbcTemplate.query(
+				"select li.* from order_line_items li, orders o where li.order_id = o.id and o.user_email = ? order by o.id, li.position asc",
+				lineItemMapper,
+				user.getEmail());
+		items.forEach( itemRow -> {
+			Order order = orderIdsMap.get(itemRow.getOrderId());
+			order.addItem(itemRow.getSku(), itemRow.getPricePer(), inventoryItems(itemRow.getSerialNumbers()));
+		});
+		return orders;
+	}
+
+	private List<InventoryItem> inventoryItems(List<String> serialNumbers) {
+		return serialNumbers.stream().map(InventoryItem::new).collect(toList());
 	}
 
 	@Override

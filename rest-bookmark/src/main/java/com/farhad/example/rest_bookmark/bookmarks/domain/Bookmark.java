d43249f9860cea89aee427415d18d21461108a8c
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-15 08:28:21 +0330

    updated

--- rest-bookmark/src/main/java/com/farhad/example/rest_bookmark/bookmarks/domain/Bookmark.java
+++ rest-bookmark/src/main/java/com/farhad/example/rest_bookmark/bookmarks/domain/Bookmark.java
@@ -9,13 +9,21 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 // Notice that Bookmark is not public. It is package-private scoped class. It is supposed to be used by BookmarkService 
 // only and is hidden from outside the package.
-
+//
+// Instead of manually setting the createdAt and updatedAt values, we can leverage some of the JPA and Spring Data JPA 
+// features to automatically set these values while inserting or updating the entities.
+// Using @PrePersist and @PreUpdate
+// or
+// Using Spring Data JPA’s @CreatedDate and @LastModifiedDate
 @Data
 @Entity
 @Table(name = "bookmarks")
@@ -29,9 +37,21 @@ class Bookmark {;
 	@Column(nullable = false)
 	private String url;
 	@Column(name =  "created_at",nullable = false, updatable = false)
+	@CreatedDate
 	private Instant createdAt;
 	@Column(name = "updated_at", insertable = false )
+	@LastModifiedDate
 	private Instant updatedAt;
+
+	// @PreUpdate
+	// @PrePersist
+	// public void updateTimeStamps() {
+	// 	this.updatedAt = Instant.now();
+	// 	if(this.createdAt == null) {
+	// 		this.createdAt = Instant.now();
+	// 	}
+	// }
+
 	public Bookmark(String title, String url, Instant createdAt) {
 		this.title = title;
 		this.url = url;

6890cbb593535dd65d1db7b8255b2b0629dbfdb8
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-23 16:50:00 +0330

    updated

--- shop-demo/src/main/java/com/farhad/example/shop_demo/cart/internal/ShoppingCartDomainService.java
+++ shop-demo/src/main/java/com/farhad/example/shop_demo/cart/internal/ShoppingCartDomainService.java
@@ -1,11 +1,16 @@
 package com.farhad.example.shop_demo.cart.internal;
 
+import static java.util.Collections.emptyList;
+import static java.util.Objects.requireNonNull;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Component;
 
+import com.farhad.example.shop_demo.cart.api.Article;
 import com.farhad.example.shop_demo.cart.api.ArticleId;
 import com.farhad.example.shop_demo.cart.api.ShoppingCart;
+import com.farhad.example.shop_demo.cart.api.ShoppingCartItem;
 import com.farhad.example.shop_demo.cart.api.ShoppingCartService;
 import com.farhad.example.shop_demo.cart.api.UserId;
 import com.farhad.example.shop_demo.cart.internal.database.api.ShoppingCartDatabase;
@@ -24,32 +29,60 @@ public class ShoppingCartDomainService implements ShoppingCartService {
 
     @Override
     public void addArticle(UserId userId, ArticleId articleId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addArticle'");
+        requireNonNull(userId);
+        requireNonNull(articleId);
+
+        ShoppingCart cart = database.loadShoppingCartByUserId(userId)
+                                .orElse(createNewShoppingCart(userId));
+        Article article = database.loadArticle(articleId)
+                                .orElseThrow(() -> new RuntimeException("Article not found: " + articleId));
+        cart.addItem(
+            new ShoppingCartItem(
+                        articleId, 
+                        1, 
+                        article.getPricrInCents()));                
+        database.saveShoppingCart(cart);
+    }
+
+    private ShoppingCart createNewShoppingCart(UserId userId) {
+        ShoppingCart cart = ShoppingCart.createNew(userId, emptyList());
+        return database.saveShoppingCart(cart);
     }
 
     @Override
     public void removeArticle(UserId userId, ArticleId articleId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeArticle'");
+        requireNonNull(userId);
+        requireNonNull(articleId);
+
+        ShoppingCart cart = database.loadShoppingCartByUserId(userId)
+                                .orElseThrow(() -> new RuntimeException("Cart not found for user: " + userId));
+        cart.removeItem(articleId);
+        database.saveShoppingCart(cart);
     }
 
     @Override
     public void resetCart(UserId userId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'resetCart'");
+        requireNonNull(userId);
+        ShoppingCart cart = database.loadShoppingCartByUserId(userId)
+                                .orElseThrow(() -> new RuntimeException("Cart not found for user: " + userId));
+        cart.reset();
+        database.saveShoppingCart(cart)            ;
     }
 
     @Override
     public void checkout(UserId userId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'checkout'");
+        requireNonNull(userId);
+        ShoppingCart cart = database.loadShoppingCartByUserId(userId)
+                                .orElseThrow(() -> new RuntimeException("Cart not found for user: " + userId));
+        shoppingCartEventPublisher.publishCheckoutEvent(cart);            
+        resetCart(userId);
     }
 
     @Override
     public ShoppingCart geShoppingCart(UserId userId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'geShoppingCart'");
+        requireNonNull(userId);
+        return database.loadShoppingCartByUserId(userId)
+                                .orElseThrow(() -> new RuntimeException("Cart not found for user: " + userId));
     }
     
 }

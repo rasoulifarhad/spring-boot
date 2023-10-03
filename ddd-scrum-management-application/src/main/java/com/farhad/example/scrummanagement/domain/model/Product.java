5f15aabfb5111a0f30906c08e81bdcae22eec4ef
farhad rasouli <rasouli.farhad@gmail.com>
2023-10-03 04:23:19 +0330

    updated

--- ddd-scrum-management-application/src/main/java/com/farhad/example/scrummanagement/domain/model/Product.java
+++ ddd-scrum-management-application/src/main/java/com/farhad/example/scrummanagement/domain/model/Product.java
@@ -1,13 +1,29 @@
 package com.farhad.example.scrummanagement.domain.model;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
-public class Product  extends ConcurrencySafeEntity{
-    
+public class Product extends ConcurrencySafeEntity {
+
     private ProductId productId;
 
     private String description;
     private String name;
-    private TenantId tenantId; 
+    private TenantId tenantId;
+
+    public void planBacklogItem( String aSummary, String aCategory,
+            BacklogItemType aType, StoryPoints aStoryPoints) {
+    }
+
+    public void scheduleRelease(String aName, String aDescription,
+                    LocalDate aBegins, LocalDate anEnds) {
+
+    }
+
+    public void scheduleSprint(String aName, String aGoals, 
+                    LocalDate aBegins, LocalDate anEnds){
+
+    }
 }

e71b38d8f27b3de166b44b035f10c0fb97428909
farhad rasouli <rasouli.farhad@gmail.com>
2023-10-06 22:54:25 +0330

    updated

--- ddd-batch-payroll-system/src/test/java/com/farhad/example/batchpayroll/domain/model/employee/HourlyEmployee.java
+++ ddd-batch-payroll-system/src/test/java/com/farhad/example/batchpayroll/domain/model/employee/HourlyEmployee.java
@@ -1,5 +1,9 @@
 package com.farhad.example.batchpayroll.domain.model.employee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HourlyEmployee extends Employee{
     
+    private List<TimeCard> timeCards = new ArrayList<>();
 }
---
+++ ddd-batch-payroll-system/src/test/java/com/farhad/example/batchpayroll/domain/model/employee/TimeCard.java
@@ -0,0 +1,8 @@
+package com.farhad.example.batchpayroll.domain.model.employee;
+
+import java.time.LocalDate;
+
+public class TimeCard {
+    private LocalDate date;
+    private int hours;
+}

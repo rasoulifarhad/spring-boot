eccf1368db014073e03abfef2089cb19587c2cb9
farhad rasouli <rasouli.farhad@gmail.com>
2023-10-07 00:08:48 +0330

    updated

---
+++ ddd-batch-payroll-system/src/test/java/com/farhad/example/batchpayroll/domain/model/ServiceCharge.java
@@ -0,0 +1,5 @@
+package com.farhad.example.batchpayroll.domain.model;
+
+public class ServiceCharge {
+    
+}
---
+++ ddd-batch-payroll-system/src/test/java/com/farhad/example/batchpayroll/domain/model/union/UnionMember.java
@@ -0,0 +1,11 @@
+package com.farhad.example.batchpayroll.domain.model.union;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.farhad.example.batchpayroll.domain.model.ServiceCharge;
+
+public class UnionMember {
+    
+    List<ServiceCharge> serviceCharges = new ArrayList<>();
+}

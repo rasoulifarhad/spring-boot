2be4c8bc69bcea0e55806840c8c24ed4c018e3b4
farhad rasouli <rasouli.farhad@gmail.com>
2023-10-10 21:47:12 +0330

    updated

--- ddd-batch-payroll-system/src/test/java/com/farhad/example/batchpayroll/domain/model/Pay.java
+++ ddd-batch-payroll-system/src/test/java/com/farhad/example/batchpayroll/domain/model/Pay.java
@@ -1,5 +1,9 @@
 package com.farhad.example.batchpayroll.domain.model;
 
+import com.farhad.example.batchpayroll.domain.model.employee.Fee;
+
 public interface Pay {
+
+    Pay apply(Fee fee);
     
 }
--- ddd-batch-payroll-system/src/test/java/com/farhad/example/batchpayroll/domain/model/affiliation/UnionAffiliation.java
+++ ddd-batch-payroll-system/src/test/java/com/farhad/example/batchpayroll/domain/model/affiliation/UnionAffiliation.java
@@ -1,13 +1,21 @@
 package com.farhad.example.batchpayroll.domain.model.affiliation;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.farhad.example.batchpayroll.domain.model.ServiceCharge;
+import com.farhad.example.batchpayroll.domain.model.employee.Fee;
 
 public class UnionAffiliation implements Affiliation{
     
     private double dues;
     List<ServiceCharge> serviceCharges = new ArrayList<>();
+    
+    @Override
+    public Fee getFee(Date date) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFee'");
+    }
 
 }

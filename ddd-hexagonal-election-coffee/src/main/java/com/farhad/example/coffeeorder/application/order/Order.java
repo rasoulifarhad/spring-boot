1c269d1ee6cb03dd3ef63fc0c279cb18c664c0ff
Farhad rasouli <rasouli.farhad@gmail.com>
2023-10-14 03:42:13 +0330

    updated

--- ddd-hexagonal-election-coffee/src/main/java/com/farhad/example/coffeeorder/application/order/Order.java
+++ ddd-hexagonal-election-coffee/src/main/java/com/farhad/example/coffeeorder/application/order/Order.java
@@ -1,15 +1,18 @@
 package com.farhad.example.coffeeorder.application.order;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 import com.farhad.example.coffeeorder.shared.Location;
 import com.farhad.example.coffeeorder.shared.Status;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Getter
 public class Order {
 	
@@ -21,6 +24,18 @@ public class Order {
 
 	private Status status = Status.PAYMENT_EXPECTED;
 
+    public boolean canBeCancelled() {
+        return status == Status.PAYMENT_EXPECTED;
+    }
+	
+    public BigDecimal getCost() {
+        return 
+			items.stream()
+				.map(LineItem::getCost)
+				.reduce(BigDecimal::add)
+				.orElse(BigDecimal.ZERO);
+    }	
+
 	public Order markPaid() {
 		if(status != Status.PAYMENT_EXPECTED) {
 			throw new IllegalStateException("Order is Already paid");
@@ -30,6 +45,27 @@ public class Order {
 	}
 
 	public Order markBeingPrepared() {
-		return null;
+		if(status != Status.PAID) {
+			throw new IllegalStateException("Order is not paid");
+		}
+		status = Status.PREPARING;
+		return this;
 	}
+
+	public Order markPrepared() {
+		if(status != Status.PREPARING) {
+			throw new IllegalStateException("Order is not being prepared");
+		}
+		status = Status.READY;
+		return this;
+	}
+
+	public Order markTaken() {
+		if(status != Status.READY) {
+			throw new IllegalStateException("Order is not ready");
+		}
+		status = Status.TAKEN;
+		return this;
+	}
+
 }

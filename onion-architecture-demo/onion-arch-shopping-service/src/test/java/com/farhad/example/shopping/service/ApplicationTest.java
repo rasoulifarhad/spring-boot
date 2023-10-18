3500e0215a0c11c9e17f889a55f66c03f9dce076
Farhad rasouli <rasouli.farhad@gmail.com>
2023-10-18 23:40:58 +0330

    updated

---
+++ onion-architecture-demo/onion-arch-shopping-service/src/test/java/com/farhad/example/shopping/service/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.farhad.example.shopping.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}

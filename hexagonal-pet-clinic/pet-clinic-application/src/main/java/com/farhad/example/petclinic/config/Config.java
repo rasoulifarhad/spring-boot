571c4a3987e17602801c0d97ffb72ab135f03bff
Farhad rasouli <rasouli.farhad@gmail.com>
2023-10-21 04:24:44 +0330

    updated

--- hexagonal-pet-clinic/pet-clinic-application/src/main/java/com/farhad/example/petclinic/config/Config.java
+++ hexagonal-pet-clinic/pet-clinic-application/src/main/java/com/farhad/example/petclinic/config/Config.java
@@ -1,8 +1,36 @@
 package com.farhad.example.petclinic.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.farhad.example.petclinic.application.customer.CustomerService;
+import com.farhad.example.petclinic.application.pet.PetService;
+import com.farhad.example.petclinic.domain.port.outgoing.CustomerRepository;
+import com.farhad.example.petclinic.domain.port.outgoing.PetRepository;
+import com.farhad.example.petclinic.infrastructure.adapter.out.persistance.inmemory.InMemoryCustomerRepsitory;
+import com.farhad.example.petclinic.infrastructure.adapter.out.persistance.inmemory.InMemoryPetRepsitory;
 
 @Configuration
 public class Config {
-	
+
+	    @Bean
+    public PetRepository petRepository() {
+        return new InMemoryPetRepsitory();
+    }
+
+    @Bean
+    public PetService petService(PetRepository petRepository) {
+        return new PetService(petRepository);
+    }
+
+    @Bean
+    public CustomerRepository customerRepository() {
+        return new InMemoryCustomerRepsitory();
+    }
+
+    @Bean
+    public CustomerService customerService(CustomerRepository customerRepository, PetService petService) {
+        return new CustomerService(customerRepository, petService);
+    }
+
 }

0ad8ed3d16897667caaa652149bff8d26b8aa0df
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-10 02:06:10 +0330

    udated

--- realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/user/Profile.java
+++ realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/user/Profile.java
@@ -1,5 +1,6 @@
 package com.farhad.example.realworld_demo.domain.user;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 
@@ -9,6 +10,9 @@ public class Profile {
     @Embedded
     private UserName userName;
 
+    @Column(name = "bio")
+    private String bio;
+    
     @Embedded
     private Image image;
 }

ac5570c5c00456ca31cf4194f472d2cb43a2c0d0
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-10 02:10:56 +0330

    updated

--- realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/user/Profile.java
+++ realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/user/Profile.java
@@ -4,15 +4,34 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Embeddable
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
 public class Profile {
 
     @Embedded
+
     private UserName userName;
 
     @Column(name = "bio")
+
     private String bio;
-    
+
     @Embedded
     private Image image;
+
+    public Profile(UserName userName) {
+        this(userName, null, null);
+    }
+
+    
+
 }

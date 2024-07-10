57c4c9117e5714e42e66a28bb6223a9a17045263
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-10 06:22:08 +0330

    updated

--- realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/article/ArticleTitle.java
+++ realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/article/ArticleTitle.java
@@ -1,5 +1,32 @@
 package com.farhad.example.realworld_demo.domain.article;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode
 public class ArticleTitle {
 
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String slug;
+
+    static ArticleTitle of(String title) {
+        return new ArticleTitle(title, slugFromTitle(title));
+    }
+
+    private static String slugFromTitle(String title) {
+        return title;
+    }
 }

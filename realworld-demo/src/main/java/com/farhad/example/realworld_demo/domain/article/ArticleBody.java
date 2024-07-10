1e11721ea283fe2e0f12c19f73d0ba03b77968cc
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-10 06:26:28 +0330

    updated

--- realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/article/ArticleBody.java
+++ realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/article/ArticleBody.java
@@ -1,5 +1,27 @@
 package com.farhad.example.realworld_demo.domain.article;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode
+
 public class ArticleBody {
 
+    @Column(nullable = false)
+    private String body;
+
+    static ArticleBody of(String body) {
+        return new ArticleBody(body);
+    }
 }
+
--- realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/article/ArticleDescription.java
+++ realworld-demo/src/main/java/com/farhad/example/realworld_demo/domain/article/ArticleDescription.java
@@ -1,5 +1,26 @@
 package com.farhad.example.realworld_demo.domain.article;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode
+
 public class ArticleDescription {
 
+    @Column(nullable = false)
+    private String description;
+
+    static ArticleDescription of(String description) {
+        return new ArticleDescription(description);
+    }
 }
